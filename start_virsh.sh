c0474f5be9a0d7638be215bd5ace863e55075c52
Mattias Hemmingsson <matte.hemmingsson@gmail.com>
2017-01-16 09:56:45 +0100

    Adding regular virsh network

--- start_virsh.sh
+++ start_virsh.sh
@@ -5,7 +5,7 @@ sudo virt-install --name=imagecreater.ops.fareoffice.com \
  --ram=2048 \
 --vcpus=2 \
 --os-type=linux \
---extra-args='ks=https://raw.githubusercontent.com/mattiashem/cloud-image/master/ks.cfg ksdevice=ens3 ip=192.168.124.90 netmask=255.255.255.0 gateway=192.168.124.1 dns=8.8.8.8' \
+--extra-args='ks=https://raw.githubusercontent.com/mattiashem/cloud-image/master/ks.cfg ksdevice=ens3 ip=192.168.122.90 netmask=255.255.255.0 gateway=192.168.122.1 dns=8.8.8.8' \
 --disk path=/var/lib/libvirt/images/imagecreator.qcow2,format=qcow2 \
 --location=http://mirror.centos.org/centos/7/os/x86_64/ \
 --network bridge=virbr0 \
