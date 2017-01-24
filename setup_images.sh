8e69c11e42bb75fd52511787c5ff9ecf03f6fc47
Mattias Hemmingsson <matte.hemmingsson@gmail.com>
2017-01-24 15:48:07 +0100

    Update docker command

--- setup_images.sh
+++ setup_images.sh
@@ -46,7 +46,8 @@ else
    yum -y install docker-engine
    curl -L "https://github.com/docker/compose/releases/download/1.9.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
    chmod +x /usr/local/bin/docker-compose
-
+   systemctl enable docker
+   systemctl start docker
 
    echo "Stop service"
    systemctl disable firewalld 
