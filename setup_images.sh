a9347abfa7882fda5965bdeee41dd0681588deb5
Mattias Hemmingsson <matte.hemmingsson@gmail.com>
2017-01-26 08:50:57 +0100

    New docker setting

--- setup_images.sh
+++ setup_images.sh
@@ -46,6 +46,14 @@ else
    yum -y install docker-engine
    curl -L "https://github.com/docker/compose/releases/download/1.9.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
    chmod +x /usr/local/bin/docker-compose
+   ln -s /usr/local/bin/docker-compose /usr/sbin/
+
+
+   mkdir -p /etc/systemd/system/docker.service.d/
+   curl -L https://raw.githubusercontent.com/mattiashem/cloud-image/master/ks.cfg -o /etc/systemd/system/docker.service.d/docker.conf   
+
+
+   systemctl daemon-reload
    systemctl enable docker
    systemctl start docker
 
